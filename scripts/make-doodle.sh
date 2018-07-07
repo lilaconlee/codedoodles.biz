58fa5588d07f4f1a557fb16b2355e67c4ea0e93f
lilaconlee <lila.conlee@gmail.com>
2018-07-07 11:59:33 -0500

    Add scss template to creation script

--- scripts/make-doodle.sh
+++ scripts/make-doodle.sh
@@ -4,6 +4,7 @@ js="import './index.scss'"
 
 mkdir $1
 cd $1
-touch index.js index.scss
+touch index.js
 echo $js >> index.js
+cp ../scripts/scss-template.scss ./index.scss
 cp ../scripts/html-template.html ./index.html
